82500afd8e38862db4ecd61c75a665114c0f1b5a
ManhCuong173 <daomanhcuong173@gmail.com>
2020-01-12 10:50:08 +0700

    Updates

---
+++ precache-manifest.01f451609aeac0aca86b2f2e3bab5194.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fac1d07b227012fffc1e518e944011c5",
+    "url": "/shoppingcart-reactJS/index.html"
+  },
+  {
+    "revision": "5631b6b6667d697ca5f5",
+    "url": "/shoppingcart-reactJS/static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "7cb07e84ecc38167b8d6",
+    "url": "/shoppingcart-reactJS/static/js/2.034463dd.chunk.js"
+  },
+  {
+    "revision": "9b177b59f5d3908ffc5ba46d5eb0523f",
+    "url": "/shoppingcart-reactJS/static/js/2.034463dd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5631b6b6667d697ca5f5",
+    "url": "/shoppingcart-reactJS/static/js/main.e386d6c4.chunk.js"
+  },
+  {
+    "revision": "43dd0de407c48e758e6f",
+    "url": "/shoppingcart-reactJS/static/js/runtime-main.5fb1697b.js"
+  }
+]);
